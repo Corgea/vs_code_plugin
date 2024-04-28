90c781256dc8466aff5bfce48dbf8f741f70c601
Ahmad Sadeddin <asadeddin@gmail.com>
2024-04-28 00:38:40 -0700

    Fixed tree view to group by file_path

--- src/vulnerabilitiesProvider.ts
+++ src/vulnerabilitiesProvider.ts
@@ -2,9 +2,9 @@ import * as vscode from 'vscode';
 import { getCorgeaUrl, getStoredApiKey } from './tokenManager';
 import axios from 'axios';
 
-export class VulnerabilitiesProvider implements vscode.TreeDataProvider<VulnerabilityItem> {
-    private _onDidChangeTreeData: vscode.EventEmitter<VulnerabilityItem | undefined> = new vscode.EventEmitter<VulnerabilityItem | undefined>();
-    readonly onDidChangeTreeData: vscode.Event<VulnerabilityItem | undefined> = this._onDidChangeTreeData.event;
+export class VulnerabilitiesProvider implements vscode.TreeDataProvider<TreeItem> {
+    private _onDidChangeTreeData: vscode.EventEmitter<TreeItem | undefined> = new vscode.EventEmitter<TreeItem | undefined>();
+    readonly onDidChangeTreeData: vscode.Event<TreeItem | undefined> = this._onDidChangeTreeData.event;
 
     constructor(private context: vscode.ExtensionContext) {}
 
@@ -12,31 +12,63 @@ export class VulnerabilitiesProvider implements vscode.TreeDataProvider<Vulnerab
         this._onDidChangeTreeData.fire(undefined);
     }
 
-    getTreeItem(element: VulnerabilityItem): vscode.TreeItem {
+    getTreeItem(element: TreeItem): vscode.TreeItem {
         return element;
     }
 
-    async getChildren(element?: VulnerabilityItem): Promise<VulnerabilityItem[]> {
+    async getChildren(element?: TreeItem): Promise<TreeItem[]> {
         if (!element) {
             // Fetch vulnerabilities here
             const url = await getCorgeaUrl(this.context);
             const apiKey = await getStoredApiKey(this.context);
             const response = await axios.get(`${url}/api/cli/issues`, { params: { token: apiKey } });
-            return response.data.issues.map(v => {
-                const filePath = `${v.file_path}:${v.line_num} - ${v.classification}`;
-                return new VulnerabilityItem(filePath, vscode.TreeItemCollapsibleState.None, {
+            const files = new Map<string, VulnerabilityItem[]>();
+
+            response.data.issues.forEach(v => {
+                const filePath = v.file_path;
+                if (!files.has(filePath)) {
+                    files.set(filePath, []);
+                }
+                files.get(filePath).push(new VulnerabilityItem(`${v.urgency} - ${v.classification}: ${v.line_num}`, vscode.TreeItemCollapsibleState.None, {
                     command: 'vulnerabilities.showDetails',
-                    title: "Show Vulnerabiity Details",
+                    title: "Show Vulnerability Details",
                     arguments: [v]
-                });
+                }));
             });
+
+            return Array.from(files.keys()).map(filePath => 
+                new FileItem(filePath, files.get(filePath))
+            );
+        } else if (element instanceof FileItem) {
+            return element.children;
         } else {
             return [];
         }
     }
 }
 
-class VulnerabilityItem extends vscode.TreeItem {
+class TreeItem extends vscode.TreeItem {
+    constructor(
+        public readonly label: string,
+        public readonly collapsibleState: vscode.TreeItemCollapsibleState
+    ) {
+        super(label, collapsibleState);
+    }
+}
+
+class FileItem extends TreeItem {
+    children: VulnerabilityItem[];
+
+    constructor(
+        public readonly label: string,
+        children: VulnerabilityItem[]
+    ) {
+        super(label, vscode.TreeItemCollapsibleState.Collapsed);
+        this.children = children;
+    }
+}
+
+class VulnerabilityItem extends TreeItem {
     constructor(
         public readonly label: string,
         public readonly collapsibleState: vscode.TreeItemCollapsibleState,

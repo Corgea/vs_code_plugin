1b4596a14eadd0e65b77bd1d46d8340fd67880a1
Ibrahim Rahhal <ibrahim.rahhal3636@gmail.com>
2025-03-08 17:41:29 +0300

    Adding Scanning

--- src/services/scanningService.ts
+++ src/services/scanningService.ts
@@ -5,13 +5,19 @@ import ContextManager from "../utils/contextManager";
 import * as os from "os";
 import * as path from "path";
 import { execSync } from "child_process";
+import StorageManager, { StorageKeys, StorageSecretKeys } from "../utils/storageManager";
 
 export default class scanningService {
   
+  /*
+    Scan cammand should only scan changed files unless the projecty doesnt have any previous scan results
+    Options to be (forced full scan)
+    before commit (git hooks integration), warning...
+  */
   @OnCommand("corgea.scan")
   public static async scanProject() {
     const pythonPath = scanningService.getPythonPath();
-
+    const corgeaPath = scanningService.getCorgeaPath();
     if (!pythonPath) {
       vscode.window.showErrorMessage("Could not determine Python path.");
       return;
@@ -23,22 +29,59 @@ export default class scanningService {
       scanningService.installCorgea(pythonPath.fsPath);
     }
 
-    // Execute corgea scan
-    const command = `${pythonPath.fsPath} -m corgea scan`;
-    TerminalManager.executeCommand(command);
+    // Set environment variables for Corgea CLI
+    const corgeaUrl = await StorageManager.getValue(StorageKeys.corgeaUrl);
+    const corgeaToken = await StorageManager.getSecretValue(StorageSecretKeys.corgeaApiKey);
+
+    if (!corgeaUrl || !corgeaToken) {
+      vscode.window.showErrorMessage("Corgea URL or API Key not set.");
+      return;
+    }
+
+    // Execute corgea scan with environment variables
+    const command = `${corgeaPath?.fsPath} scan`;
+    let envCommand = "";
+    if (os.platform() === "win32") {
+      envCommand = `set CORGEA_URL="${corgeaUrl}"`;
+    } else {
+      envCommand = `export CORGEA_URL="${corgeaUrl}"`;
+    }
+    TerminalManager.executeCommand(envCommand);
+    TerminalManager.executeCommand(`${corgeaPath?.fsPath} login ${corgeaToken}`);
+    // Clear the terminal before executing the scan command
+    if (os.platform() === "win32") {
+      TerminalManager.executeCommand("cls");
+    } else {
+      TerminalManager.executeCommand("clear");
+    }
+    TerminalManager.executeCommand(`${corgeaPath?.fsPath} scan`);
   }
 
   private static getPythonPath(): vscode.Uri | null {
     const contextUri = ContextManager.getContext().extensionUri;
 
     if (os.platform() === "win32") {
-      return vscode.Uri.joinPath(contextUri, "./assets/runtimes/python/myenv/Scripts/python.exe");
+      return vscode.Uri.joinPath(contextUri, "./assets/runtimes/python/myvenv/Scripts/python.exe");
     } else if (os.platform() === "darwin") {
-      return vscode.Uri.joinPath(contextUri, "./assets/runtimes/python/myenv/bin/python3");
+      return vscode.Uri.joinPath(contextUri, "./assets/runtimes/python/myvenv/bin/python3");
     } else if (os.platform() === "linux") {
-      return vscode.Uri.joinPath(contextUri, "./assets/runtimes/python/myenv/bin/python3");
+      return vscode.Uri.joinPath(contextUri, "./assets/runtimes/python/myvenv/bin/python3");
     }
     
+    return null;
+  }
+
+  private static getCorgeaPath(): vscode.Uri | null {
+    const pythonPath = scanningService.getPythonPath();
+    if (pythonPath) {
+      let corgeaPath = path.dirname(pythonPath.fsPath);
+      if (os.platform() === "win32") {
+        corgeaPath = path.join(corgeaPath, "./corgea.exe");
+      } else {
+        corgeaPath = path.join(corgeaPath, "./corgea");
+      }
+      return vscode.Uri.file(corgeaPath);
+    }
     return null;
   }
 
@@ -53,7 +96,7 @@ export default class scanningService {
 
   private static installCorgea(pythonPath: string): void {
     try {
-      execSync(`${pythonPath} -m pip install corgea`, { stdio: "inherit" });
+      execSync(`${pythonPath} -m pip install corgea-cli`, { stdio: "inherit" });
       vscode.window.showInformationMessage("Corgea installed successfully!");
     } catch (error) {
       vscode.window.showErrorMessage("Failed to install Corgea.");
